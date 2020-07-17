8def38eb22a7a2ccd4d746e569e7243000c4e417
yxy1052176134 <62056957+yxy1052176134@users.noreply.github.com>
2020-07-17 13:13:45 +0800

    优化代码
    一点点修改

--- 04_QEvent/mylabel.h
+++ 04_QEvent/mylabel.h
@@ -13,8 +13,8 @@ protected:
     void mousePressEvent(QMouseEvent *ev) override;//实现虚函数
     void mouseReleaseEvent(QMouseEvent *ev) override;
     void mouseMoveEvent(QMouseEvent *ev) override;
-    void enterEvent(QEvent *e);
-    void leaveEvent(QEvent *e);
+    void enterEvent(QEvent *e) override;
+    void leaveEvent(QEvent *e) override;
 
 signals:
 
